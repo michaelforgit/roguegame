0b328360441ceb0a73fc7234fb659a9bc710e402
Michael <michaelforemail@protonmail.com>
2024-08-07 21:34:58 -0500

    initial template

---
+++ src/Game.cs
@@ -0,0 +1,16 @@
+namespace roguegame;
+
+using Godot;
+
+public partial class Game : Control {
+  public Button TestButton { get; private set; } = default!;
+  public int ButtonPresses { get; private set; }
+
+  public override void _Ready()
+    => TestButton = GetNode<Button>("%TestButton");
+
+  public void OnTestButtonPressed() {
+    ButtonPresses++;
+    GD.Print(ButtonPresses);
+  }
+}
